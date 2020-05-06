b14a00083b7a16f3e10b1f06c7cdff4575a8e53a
Ezequiel Actis Grosso <eactisgrosso@gmail.com>
2020-05-06 14:18:05 -0300

    Test new action - 13

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-echo "Installing Nodejs ${INPUT_NODE-VERSION}"
-curl -sL https://deb.nodesource.com/setup_${INPUT_NODE-VERSION} > setup_${INPUT_NODE-VERSION} && \
-	chmod +x setup_${INPUT_NODE-VERSION} && \
-	./setup_${INPUT_NODE-VERSION} && \
+printenv
+
+echo "Installing Nodejs ${INPUT_NODE_VERSION}"
+curl -sL https://deb.nodesource.com/setup_${INPUT_NODE_VERSION} > setup_${INPUT_NODE_VERSION} && \
+	chmod +x setup_${INPUT_NODE_VERSION} && \
+	./setup_${INPUT_NODE_VERSION} && \
 	apt install nodejs -y
 
 output=$(samdev build & samdev deploy --stack-name ${INPUT_STACK-NAME} 2>&1)
