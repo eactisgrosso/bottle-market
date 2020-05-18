232ba255394419fbaae5774f501f04650c1c9cd0
Ezequiel Actis Grosso <eactisgrosso@gmail.com>
2020-05-18 02:35:19 -0300

    Disabling guards

--- packages/api/shop/services/user/user.resolver.ts
+++ packages/api/shop/services/user/user.resolver.ts
@@ -22,7 +22,7 @@ export class UserResolver {
 
   private readonly items: UserDTO[] = loadUsers();
 
-  @UseGuards(GraphqlAuthGuard)
+  // @UseGuards(GraphqlAuthGuard)
   @Mutation(() => UserDTO, { description: "Log In User" })
   async signMeUp(
     @Args("signUpInput") signUpInput: CreateUserInput
@@ -60,7 +60,7 @@ export class UserResolver {
     return await this.items[0];
   }
 
-  @UseGuards(GraphqlAuthGuard)
+  // @UseGuards(GraphqlAuthGuard)
   @Query(() => UserDTO)
   async me(@Args("id") id: string): Promise<UserDTO> {
     let user = new UserDTO();
@@ -90,7 +90,7 @@ export class UserResolver {
     return user;
   }
 
-  @UseGuards(GraphqlAuthGuard)
+  // @UseGuards(GraphqlAuthGuard)
   @Mutation(() => UserDTO, { description: "Update User" })
   async updateMe(@Args("meInput") meInput: UpdateUserInput): Promise<UserDTO> {
     const user = await this.repository.load(meInput.id);
