33b7d9feca87c21700f7b5750c63af5ab76b5e26
Ezequiel Actis Grosso <eactisgrosso@gmail.com>
2020-08-05 23:41:26 -0300

    GraphQL context populated from request

--- packages/api/retailer/app.module.ts
+++ packages/api/retailer/app.module.ts
@@ -1,17 +1,17 @@
-import { Module } from "@nestjs/common";
-import { GraphQLModule } from "@nestjs/graphql";
-import { ConfigModule } from "@nestjs/config";
-import { KnexModule } from "@nestjsplus/knex";
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { KnexModule } from '@nestjsplus/knex';
 
-import { IAppModule } from "../app";
-import { AuthzModule } from "../common/auth/authz.module";
-import { UserModule } from "../common/services/user/user.module";
+import { IAppModule } from '../app';
+import { AuthzModule } from '../common/auth/authz.module';
+import { UserModule } from '../common/services/user/user.module';
 
-import { LocationModule } from "./services/location/location.module";
-import { StoreModule } from "./services/store/store.module";
-import { StoreProductModule } from "./services/store_product/store_product.module";
-import { DeliveryModule } from "./services/delivery_area/delivery_area.module";
-import { ParameterStore } from "../common/config/parameterStore";
+import { LocationModule } from './services/location/location.module';
+import { StoreModule } from './services/store/store.module';
+import { StoreProductModule } from './services/store_product/store_product.module';
+import { DeliveryModule } from './services/delivery_area/delivery_area.module';
+import { ParameterStore } from '../common/config/parameterStore';
 
 @Module({
   imports: [
@@ -26,9 +26,10 @@ import { ParameterStore } from "../common/config/parameterStore";
       isGlobal: true,
     }),
     GraphQLModule.forRoot({
-      path: "/retailer/graphql",
+      path: '/retailer/graphql',
       installSubscriptionHandlers: true,
       autoSchemaFile: true,
+      context: ({ request }) => ({ req: request }),
     }),
     KnexModule.registerAsync({
       useFactory: async () => (await ParameterStore.getInstance()).dbConfig,
