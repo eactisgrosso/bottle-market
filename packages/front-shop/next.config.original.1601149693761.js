0484068b3ece1f22d8ce8858fd84c9c8bc0212bc
Ezequiel Actis Grosso <eactisgrosso@gmail.com>
2020-09-26 17:01:09 -0300

    Re-added next config original

---
+++ packages/front-shop/next.config.original.1601149693761.js
@@ -0,0 +1,25 @@
+const { withPlugins } = require('next-compose-plugins');
+const withOptimizedImages = require('next-optimized-images');
+const withTM = require('next-transpile-modules')(['@bottle-market/common']);
+
+// next.js configuration
+const nextConfig = {
+  target: 'serverless',
+  env: {
+    STRIPE_PUBLIC_KEY: 'your_stripe_public_key',
+    API_URL: process.env.REACT_APP_API_URL,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
+    AUTH0_CALLBACK: process.env.AUTH0_CALLBACK,
+    REACT_APP_MAPS_API_KEY: process.env.REACT_APP_MAPS_API_KEY,
+  },
+  webpack: (config, options) => {
+    config.resolve.modules.push(__dirname);
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+};
+
+module.exports = withPlugins([withTM, withOptimizedImages], nextConfig);
